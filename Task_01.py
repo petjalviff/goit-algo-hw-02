ee8beb21f54e4d78a39ef0ce3b64b69b5bbc69bb
Andrii Petryshyn <petjalviff@gmail.com>
2024-04-28 19:43:22 +0300

    algo_HW2 task 1,2

---
+++ Task_01.py
@@ -0,0 +1,33 @@
+from queue import Queue
+
+class ServiceCenter: #клас що імітує роботу сервісного центру
+    def __init__(self):
+        self.aplications = Queue()
+
+    def generate_request(self,numb): #функція для генерування та додавання заявки в чергу
+        numb_apl=numb
+        for i in range(numb_apl):
+            self.aplications.put(f"Заявка #{i+1}")
+
+    def process_request(self): #функція яка обробляє та видаляє заявки. Якщо черга порожня виводить відповідне повідмлення.
+        if not self.aplications.empty():
+            while not self.aplications.empty():
+                current_aplication = self.aplications.get()
+                print(f"Обробляється {current_aplication}")
+        else:
+            print("Черга порожня")    
+
+# Створюємо сервісний центр
+center=ServiceCenter()
+
+#Головний цикл програми:
+while True:
+    print("привіт це чат бот сервісного центру!")
+    command=int(input("Введідть команду: \n1- запуск сервісного центру, \n2-завершити роботу."))
+    if command==1:
+        center.generate_request(int(input("Введи кількість заявок, яку бажаєте обробити сервісним центром, число має бути цілим та додатнім:")))
+        center.process_request()
+        print("Сервісний центр обробив всі заявки")
+        print("*"*30)
+    if command==2:
+        break
---
+++ Task_02.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def is_palindrome(input_string):
+    # приведення рядка у нижній регістр та видалення пробілів
+    string = input_string.lower().replace(" ", "")
+    
+    # Ініціалізація черги для зберігання символів рядка
+    d_queue = deque()
+    
+    # Додавання кожного символу рядка до черги
+    for char in string:
+        d_queue.append(char)
+    
+    # Порівняння символів з обох кінців черги
+    while len(d_queue) > 1:
+        if d_queue.popleft() != d_queue.pop():
+            return print("Ні, введений рядок не є паліндромом")
+    
+    # Якщо всі символи збігаються, рядок є паліндромом
+    return print("Так, введений рядок є паліндромом")
+
+# Приклад використання
+is_palindrome(input("Введіть речення або слово (наприклад: кит на морі романтик):"))
+
+
